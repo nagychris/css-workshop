3711a555827da43c9b66cae77cb952655c62a34f
Christopher Nagy <christopher_nagy@icloud.com>
2025-01-17 19:32:46 +0100

    add todo

--- task3-tailwind/src/App.tsx
+++ task3-tailwind/src/App.tsx
@@ -6,6 +6,8 @@ function App() {
 
     return (
         <div>
+            // TODO render some kind of list of cards with some dummy data
+            <Card />
             <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={() => setCount(count + 1)}
